8a9414c82daa14458b04a0dd246222a37561f0c8
gajulakranthi <102822548+gajulakranthi@users.noreply.github.com>
2022-10-12 19:23:54 +0800

    Update Add.java
    one more semicolon added

--- src/main/java/org/example/Add.java
+++ src/main/java/org/example/Add.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Add {
-    System.out.println("This is from feature 2 added ;");
+    System.out.println("This is from feature 2 added ;;");
     int add(int a,int b){
     return a+b;
     }
